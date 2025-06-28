ad93f80c55f9afbaa12a68828a1d7ca7319ebea4
135ahmedsayed <ahmedshkra246@gmail.com>
2025-06-28 07:33:33 +0300

    Q2 Solve

--- Assignment_04/Program.cs
+++ Assignment_04/Program.cs
@@ -16,6 +16,19 @@
             PassByReference(ref value);
             Console.WriteLine(value);
             #endregion
+            //_______________________//
+            #region Q2
+            // Passing by Reference Value Type:Any Changes made in Functions don't affect the orginal type
+            int[] values = new int[] { 1, 2, 3, 4 };
+            Console.Write("Print by Passing by Reference Value : ");
+            Console.WriteLine(PassByReferenceValue(values));
+            foreach (int i in values) Console.WriteLine(i);
+            // Passing by Reference Value Type:Any Changes made in Functions do affect the orginal type
+            Console.Write("Print by Passing by Reference ref : ");
+            Console.WriteLine(PassByReferenceRef(ref values));
+            foreach (int i in values) Console.WriteLine(i);
+            Console.WriteLine("\n");
+            #endregion
             Console.ReadKey();
         }
         //_________________//
@@ -27,6 +40,21 @@
         public static void PassByReference(ref int pass)
         {
             pass = 10; // This change will affect the original variable
+        }
+        #endregion
+        //_________________//
+        #region with Q2
+        public static int PassByReferenceValue(int[] pass)
+        {
+            pass = new int[] { 0, 0, 0, 0 };
+            int sum = pass[1] + pass[2]; // Execute done but the original array remains unchanged
+            return sum;
+        }
+        public static int PassByReferenceRef(ref int[] pass)
+        {
+            pass = new int[] { 1, 1, 1, 1 };
+            int sum = pass[1] + pass[2]; // Execute done and the original array changed
+            return sum;
         }
         #endregion
     }
